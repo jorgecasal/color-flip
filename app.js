aae4d3279050a96b15262379178c10853b91f005
Jorge Casal <casaldelacruz@gmail.com>
2020-09-16 10:10:08 +0200

    app.js working

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function(){
-    const randomNumber = getRandomNumber();
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+    let hexOnTheFly = '#';
+    for(let i = 0; i < 6; i++){
+        hexOnTheFly += hex[getRandomNumber()];
+    }
+    color.textContent = hexOnTheFly;
+    document.body.style.backgroundColor = hexOnTheFly;
 })
 
 function getRandomNumber(){
-    return Math.floor(Math.random()*colors.length);
+    return Math.floor(Math.random()*hex.length);
 }
